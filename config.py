830c6150cf04b2c3a05632aa067f5e23af2bd897
Crypto B <103379312+CryptoBusher@users.noreply.github.com>
2023-03-05 01:08:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ refs_to_register = 1
 threads = 1
 wait_element_sec = 15
 referrals = [
-    ('your_backpack_ref_link', 'your_backpack_invite_code')
+    ('your_backpack_ref_link1', 'your_backpack_invite_code1'),
+    ('your_backpack_ref_link2', 'your_backpack_invite_code2'),
+    ('your_backpack_ref_link3', 'your_backpack_invite_code3')
 ]
